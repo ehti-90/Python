0e66da43f5f4a263947aacd34ba65fac3eb2afd7
Ehtisham Hassan <ehtishamh99@gmail.com>
2026-08-04 02:40:01 +0500

    Using __file__ to build script-relative file paths.

--- SMTP_Module/weekly_motivation_mail/main.py
+++ SMTP_Module/weekly_motivation_mail/main.py
@@ -5,15 +5,17 @@ from dotenv import load_dotenv
 import os
 
 
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Load variables from .env
-load_dotenv()
+load_dotenv(os.path.join(SCRIPT_DIR, ".env"))
+
 
 MY_EMAIL = os.getenv("my_email")
 APP_PASSWORD = os.getenv("APP_PASSWORD")
 RECEIPENT = os.getenv("my_email") #IF YOU WANT TO SEND IT TO YOURSELF 
 
-with open(file=r"practice projects\SMTP_Module\weekly_motivation_mail\quotes.txt",mode="r") as data:
+with open(file=os.path.join(SCRIPT_DIR, "quotes.txt") ,mode="r") as data:
     all_data = data.readlines() #   gives us all lines in a list 
     
 # # # # # # # # cleaned_quotes = [line.strip() for line in all_data] # takes out \n \t thing at end of each line
@@ -24,11 +26,11 @@ quote = random.choice(all_data)
 date = dt.datetime.now()
 weekday = date.weekday()
 
-if weekday == 6:
-    with smtp.SMTP(host="smtp.gmail.com", port=587) as connection: #google host server and  STARTTLS (Port 587)
-        connection.starttls()
-        connection.login(user=MY_EMAIL,password=APP_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,to_addrs=RECEIPENT,msg=f"Subject:Listen Nigga \n\n {quote} ")
+
+with smtp.SMTP(host="smtp.gmail.com", port=587) as connection: #google host server and  STARTTLS (Port 587)
+    connection.starttls()
+    connection.login(user=MY_EMAIL,password=APP_PASSWORD)
+    connection.sendmail(from_addr=MY_EMAIL,to_addrs=RECEIPENT,msg=f"Subject: Listen Boy!!! \n\n {quote} ")
 
     
 
